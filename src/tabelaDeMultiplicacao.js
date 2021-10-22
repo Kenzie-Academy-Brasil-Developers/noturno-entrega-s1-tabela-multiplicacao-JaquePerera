3c87cad9e40c16e75025ed6736599e73394c8dc9
Jaqueline Pereira Silva <jaque.perera@gmail.com>
2021-10-22 19:36:30 -0300

    initial commit index.html e tabelaDeMultiplicacao.js

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,18 @@
-function nomeDaFuncao() { 
+
+function tabelaMult(n) { 
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
+    var x = new Array(n);
+    x = [];
+    let multiplicador = ''
+    // todo: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
+    for (let linha = 0; linha <= n; linha++) {
+        let count =0
+        x[linha] = new Array(n);
+        for(let coluna =0; coluna <= n; coluna++){
+            x[linha][coluna]= linha * count
+            count +=1
+        }
     }
     console.table(x)
 }
+console.log(tabelaMult(12))
